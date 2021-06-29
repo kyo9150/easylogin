1a2fc4f2702ded0551afeff28018337bd30c07f7
kyo9150 <kyo9150@gmail.com>
2021-06-29 11:28:08 +0900

    アップロード

---
+++ src/main/java/com/example/easylogin/Easylogin1Application.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easylogin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easylogin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easylogin/Easylogin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easylogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Easylogin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
